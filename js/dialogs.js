7a5c8ffe71e242e59669700a0697a463282fccc2
Nathan Nieman <nnieman@bitbucket.org>
2013-06-08 14:34:39 -0400

    Add new game dialog.

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,3 +1,38 @@
+function splash () {
+    $("#splash").modal({
+        onOpen: function (dialog) {
+            dialog.overlay.fadeIn(400);
+            dialog.container.fadeIn(400);
+            dialog.data.fadeIn(400);
+        },
+        onClose: function (dialog) {
+            dialog.overlay.fadeOut(400);
+            dialog.container.fadeOut(400);
+            dialog.data.fadeOut(400, function () {
+                $.modal.close();
+            });
+        },
+        close: false
+    });
+}
+
+function newGame () {
+    $("#newGame").modal({
+        onOpen: function (dialog) {
+            dialog.overlay.fadeIn(400);
+            dialog.container.fadeIn(400);
+            dialog.data.fadeIn(400);
+        },
+        onClose: function (dialog) {
+            dialog.overlay.fadeOut(400);
+            dialog.container.fadeOut(400);
+            dialog.data.fadeOut(400, function () {
+                $.modal.close();
+            });
+        }
+    });
+}
+
 function doneDialog (game) {
   $("#winText").html("Winner is the " + game.winner.color + " player!");
   $("#done-dialog").dialog({
